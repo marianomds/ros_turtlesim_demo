c41c3a0a6e76551a9b55f59619cb30c6a25e2be1
Mariano <mmarufodasilva@est.frba.utn.edu.ar>
2017-10-04 13:49:53 -0300

    Added angular and linear movement to the desired point coordinates in move() method.

--- src/demo.py
+++ src/demo.py
@@ -46,9 +46,61 @@ class turtlebot():
         goal_pose.x = point[0]
         goal_pose.y = point[1]
 
-        print("angular: %f" % self.get_angular_distance(goal_pose.x, goal_pose.y))
-        print("linear: %f" % self.get_linear_distance(goal_pose.x, goal_pose.y))
+        # Create the velocity vector atribute
+        self.vel_msg = Twist()
+
+        # Linear velocity only in the x-axis
+        self.vel_msg.linear.y = 0
+        self.vel_msg.linear.z = 0
+
+        # Angular velocity only in the z-axis
+        self.vel_msg.angular.x = 0
+        self.vel_msg.angular.y = 0
         
+        # The turtle will first rotate => linear velocity = 0
+        self.vel_msg.linear.x = 0
+
+        # The loop will remain until the angular distance starts to increase again
+        prev_angular_distance = 100
+        angular_distance = self.get_angular_distance(goal_pose.x, goal_pose.y)
+        while abs(angular_distance) <= abs(prev_angular_distance) and abs(angular_distance) > 0.001:
+
+            # Porportional Controller
+            self.vel_msg.angular.z = 2 * angular_distance
+
+            # Publish the velocity
+            self.velocity_publisher.publish(self.vel_msg)
+            self.rate.sleep()
+
+            # Re-calculate the distance
+            prev_angular_distance = angular_distance
+            angular_distance = self.get_angular_distance(goal_pose.x, goal_pose.y)
+
+
+        # The turtle will now translate => angular velocity = 0
+        self.vel_msg.angular.z = 0
+
+        # The loop will remain until the linear distance starts to increase again
+        prev_linear_distance = 100
+        linear_distance = self.get_linear_distance(goal_pose.x, goal_pose.y)
+        while linear_distance <= prev_linear_distance and linear_distance != 0:
+
+            # Porportional Controller
+            self.vel_msg.linear.x = 5 * linear_distance 
+
+            # Publish the velocity
+            self.velocity_publisher.publish(self.vel_msg)
+            self.rate.sleep()
+
+            # Re-calculate the distance
+            prev_linear_distance = linear_distance
+            linear_distance = self.get_linear_distance(goal_pose.x, goal_pose.y)
+        
+        # Stop turtle
+        self.vel_msg.linear.x = 0
+        self.vel_msg.angular.z =0
+        self.velocity_publisher.publish(self.vel_msg)
+
 
 if __name__ == '__main__':
     try:
