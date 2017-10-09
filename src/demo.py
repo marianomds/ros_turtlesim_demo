b1d99fb7677c4238852ec0fd843c1855a096957c
Mariano <mmarufodasilva@est.frba.utn.edu.ar>
2017-10-09 17:13:59 -0300

    Working version for drawing with two turtles

--- src/demo.py
+++ src/demo.py
@@ -4,7 +4,7 @@ import rospy
 from geometry_msgs.msg  import Twist
 from turtlesim.msg import Pose
 from std_srvs.srv import Empty
-from turtlesim.srv import TeleportAbsolute, SetPen
+from turtlesim.srv import Spawn, Kill
 from math import pow,atan2,sqrt
 import csv
 
@@ -25,28 +25,26 @@ def getpoints(filename):
 class turtlebot():
 
 	# Initialization funcion
-    def __init__(self, ini_point): # The init funcion receives the initial point for positioning the turtle
-    	# Create the velocity publisher
-        self.velocity_publisher = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size=10)
+    def __init__(self, ini_point, turtlen): # The init funcion receives the initial point for positioning the turtle
+        
+        while True:
+            try: # Try to create a new turtle with the name stored in turtlen
+                rospy.wait_for_service('spawn')
+                spawner = rospy.ServiceProxy('spawn', Spawn)
+                spawner(ini_point[0],ini_point[1], 0, turtlen)
+                break
+            except rospy.service.ServiceException: # If there already exists a turtle with that name, kill it
+                rospy.wait_for_service('kill')
+                killer = rospy.ServiceProxy('kill', Kill)
+                killer(turtlen)
+
+        # Create the velocity publisher
+        self.velocity_publisher = rospy.Publisher(turtlen + '/cmd_vel', Twist, queue_size=10)
 
         # Create the position subscriber
-        self.pose_subscriber = rospy.Subscriber('turtle1/pose', Pose, self.callback)
+        self.pose_subscriber = rospy.Subscriber(turtlen + '/pose', Pose, self.callback)
         self.pose = Pose()
         self.rate = rospy.Rate(10) # Refresh frequency [Hz]
-
-        # Turn off the pen
-        rospy.wait_for_service('turtle1/set_pen')
-        set_pen = rospy.ServiceProxy('turtle1/set_pen', SetPen)
-        set_pen(0,0,0,0,1)
-
-        # Move the turtle to the initial position
-        rospy.wait_for_service('turtle1/teleport_absolute')
-        teleport = rospy.ServiceProxy('turtle1/teleport_absolute', TeleportAbsolute)
-        teleport(ini_point[0],ini_point[1],0)
-
-        # Turn on the pen
-        set_pen(255,255,255,4,0)
-
 
     # Callback function formatting the pose value received
     def callback(self, data):
@@ -144,12 +142,34 @@ if __name__ == '__main__':
         elif (ans == 'f') or (ans == 'F'):
             filename = rospy.get_param('~filename') # run the node with parameter: _filename:=full_path_filename
             points = getpoints(filename) # Use the custom figure coordinates
+        else:
+            print('Input Error')
+            exit()
 
-        # Instance a turtle objet       
-        x = turtlebot(points[0])
+        # The figure can be drawn by one or two turtles
+        ans = raw_input("Would you like to use one (1) or two (2) turtles?:")
+        if (ans == '1'):
+            n_turtles = 1
+        elif (ans == '2'): # If two turtles will be used:
+            n_turtles = 2
+            reversed_points = points[-1:len(points)/2-1:-1] # the second turtle will go from the end and backwards
+            points = points[0:len(points)/2+1] # and the first turtle will go from the beggining and forwards
+        else:
+            print('Input Error')
+            exit()
 
-        # Move the turtle object to each point
+
+        # Instance a turtle objet named turtle1
+        x = turtlebot(points[0],'turtle1')
+
+        # Move turtle1 to each point
         for p in points:
             x.move(p)
+
+        if (n_turtles == 2):
+            y = turtlebot(reversed_points[0],'turtle2') # Instance a second turtle objet named turtle2
+            for p in reversed_points:
+                y.move(p) # Move turtle2 to each point
+
 
     except rospy.ROSInterruptException: pass
